42a927134f4865d16be154a63de091d27953f017
adasneves127 <adasneves127@gmail.com>
2021-07-10 20:57:54 -0400

    Added "Rfne" and  white box functionallity

--- PHP/Transactions/Transaction.php
+++ PHP/Transactions/Transaction.php
@@ -7,6 +7,7 @@ $EmpLName = $_COOKIE["Lname"];
 $CoSignName = $_COOKIE["CoSignName"];
 $CoSignPriv = $_COOKIE["CoSignPriv"];
 
+
 if($EmpPriv >= 1)
 {
 }
@@ -24,6 +25,35 @@ else
         <title>Transaction In Progress</title>
     </head>
     <body>
-        <div class="RightNavBar"></div>
+        <div class="RightNavBar">
+            <p class="StatusBox" id="Status">Test</p>
+            <button class="Trans Cancel">Cancel Transaction</button>
+            <button class="Trans PLU">PLU Query</button>
+            <button class="Trans UPC">UPC Query</button>
+            <button class="Trans TendPay">Tender Payment</button>
+        </div>
+        <div style="width: 1600px;
+                    height: 900px;
+                    background: white;
+                    top: 140px;
+                    left: 40px;">
+                <pre id="TransactionItems"></pre>    
+                </div>
+        <div style="top: 100px; left: 40px;">
+            <label for="InputBox" style="Color: white;">Enter Next Code: </label>
+            <input type="text" name="InputBox" id="InputBox" >
+            <button onclick="AddItem()">Add Item</button>
+        </div>
+        
     </body>
+    <script>
+        document.getElementById("Status").innerHTML = "Ready for Next Entry";
+        function AddItem()
+        {
+            console.log(document.getElementById("InputBox").value);
+            
+            var textToAdd = document.createTextNode(document.getElementById("InputBox").value + "\n");
+            document.getElementById("TransactionItems").appendChild(textToAdd);
+        }
+    </script>
 </html>
